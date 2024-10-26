540e2ceb6537956c470bd1370393a06aa2378e09
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-26 21:30:16 +0200

    Add files via upload

---
+++ klasaStringVjezba03/klasaStringVjezba03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVjezba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova");
+            string unos = Console.ReadLine();
+
+            string sVelika = unos.ToUpper();
+            string sMala = unos.ToLower();
+            string sPrvaTri = unos.Substring(0, 3);
+            string sZadnjihPet = unos.Substring(5);
+            string s8_11 = unos.Substring(7,4);
+
+            Console.WriteLine("Velika slova:" + sVelika);
+            Console.WriteLine("Mala slova:" +sMala);
+            Console.WriteLine("Prva tri slova:" +sPrvaTri);
+            Console.WriteLine("Zadnjih pet:" +sZadnjihPet);
+            Console.WriteLine("Slova od 8-11:" +s8_11);
+            Console.ReadKey(); 
+        }
+    }
+}
